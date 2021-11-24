1f63c8cd1988625e6e1b375a50c8ab59714c348e
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-11-24 22:45:25 +0000

    added tests and comments

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,27 @@
+const eqArrays = function(arr1, arr2) {
+  [arr1, arr2].forEach(arr => {
+    if (!(arr instanceof Array)) {
+      return console.log("Function expects 2 arrays as inputs");
+    }
+  });
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  const result = eqArrays(arr1, arr2);
+  if (result) {
+    console.log(`✅ Assertion passed: ${arr1.toString()} === ${arr2.toString()}`);
+  } else {
+    console.log(`🛑 Assertion failed: ${arr1.toString()} !== ${arr2.toString()}`);
+  }
+};
+
+// ASSIGNMENT
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
@@ -9,9 +33,9 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-console.log(letterPositions(`const letterPositions = function(sentence) {
-  const results = {};
-  // logic to update results here
-  return results;
-};
-`));
+// TESTS
+const output = letterPositions('stringing');
+assertArraysEqual(output.s, [0]);
+assertArraysEqual(output.r, [2]);
+assertArraysEqual(output.n, [4, 7]);
+
