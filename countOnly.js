974fe696fb07753766bd015b12c6d2c8555a5da2
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-11-24 19:17:15 +0000

    added countOnly function

---
+++ countOnly.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(arr, shouldCount) {
+  const outputObj = {};
+  for (let element of arr) {
+    if (!shouldCount[element]) {
+      continue;
+    }
+    // If no count has been started for this element, set it to 1
+    outputObj[element] ? outputObj[element]++ : outputObj[element] = 1;
+  }
+  return outputObj;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
