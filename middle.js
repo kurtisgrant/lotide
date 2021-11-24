b0da5e902f04525b2b69c26697980b357b366dd6
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-11-24 17:59:57 +0000

    minor comment changes

--- middle.js
+++ middle.js
@@ -38,14 +38,14 @@ const middle = function(arr) {
 };
 
 assertArraysEqual(
-  middle([2, 4, 6, 8]),
+  middle([2, 4, 6, 8]), // -> [4, 6]
   [4, 6]
 );
 assertArraysEqual(
-  middle([3, 6, 9]),
+  middle([3, 6, 9]), // -> [6]
   [6]
 );
 assertArraysEqual(
-  middle(['this', 'is', 'a', 'test', 'of', 'middle.js']),
+  middle(['this', 'is', 'a', 'test', 'of', 'middle.js']), // -> ['a', 'test']
   ['a', 'test']
 );
