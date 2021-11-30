1e3d745d02926ea6cf2874c6669111263b69440f
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-11-30 01:45:55 +0000

    npm init & refactored headTest to use mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,19 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual(head([3, 4, 7]), 3);
-assertEqual(head(["Hello", "Test", "Kurtis"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([5]), 5);
+describe('#head', () => {
+
+  it('should return 3 when given [3, 4, 7]', () => {
+    assert.strictEqual(head([3, 4, 7]), 3);
+  });
+  it('should return "Hello" when given ["Hello", "Test", "Kurtis"]', () => {
+    assert.strictEqual(head(["Hello", "Test", "Kurtis"]), "Hello");
+  });
+  it('should return undefined when given []', () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it('should return 5 when given [5]', () => {
+    assert.strictEqual(head([5]), 5);
+  });
+
+});
