728e34c440ec67abfe187d547d38f280bec4ccbd
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-11-30 02:18:09 +0000

    Refactored tests to use mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,19 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe('#tail', () => {
 
-const result = tail(["Hello", "Kurtis", "Test"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Kurtis");
-assertEqual(result[1], "Test");
-const words = ["Piano", "Lipchap", "Truck"];
-tail(words);
-assertEqual(words.length, 3);
+  it('should return [4, 7] when given [3, 4, 7]', () => {
+    assert.deepEqual(tail([3, 4, 7]), [4, 7]);
+  });
+  it('should return ["Test", "Kurtis"] when given ["Hello", "Test", "Kurtis"]', () => {
+    assert.deepEqual(tail(["Hello", "Test", "Kurtis"]), ["Test", "Kurtis"]);
+  });
+  it('should not change original array', () => {
+    const arr = ["This", "is", "a", "test"];
+    const arrCopy = arr.slice(0, arr.length);
+    tail(arr);
+    assert.deepEqual(arrCopy, arr);
+  });
+
+});
