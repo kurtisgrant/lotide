6a5805e88ff3b73aeb269f30e90fc5c1b9a64c16
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-11-30 02:27:56 +0000

    Refactored tests to use mocha and chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,16 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+describe('#eqArrays', () => {
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+  it('should return false when given [3,6] and [1,6]', () => {
+    assert.isFalse(eqArrays([3, 6], [1, 6]));
+  });
+  it('should return true when given [1, 5] and [1, 5]', () => {
+    assert.isTrue(eqArrays([1, 5], [1, 5]));
+  });
+  it('should return true when given [] and []', () => {
+    assert.isTrue(eqArrays([], []));
+  });
+
+});
