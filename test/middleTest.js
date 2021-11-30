d777eb75cae02a2f73415660f389deb4212cb3e7
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-11-30 02:22:03 +0000

    Refactored tests to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,15 +1,19 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(
-  middle([2, 4, 6, 8]), // -> [4, 6]
-  [4, 6]
-);
-assertArraysEqual(
-  middle([3, 6, 9]), // -> [6]
-  [6]
-);
-assertArraysEqual(
-  middle(['this', 'is', 'a', 'test', 'of', 'middle.js']), // -> ['a', 'test']
-  ['a', 'test']
-);
+describe('#middle', () => {
+
+  it('should return [4, 7] when given [3, 4, 7, 9]', () => {
+    assert.deepEqual(middle([3, 4, 7, 9]), [4, 7]);
+  });
+  it('should return ["Test"] when given ["Hello", "Test", "Kurtis"]', () => {
+    assert.deepEqual(middle(["Hello", "Test", "Kurtis"]), ["Test"]);
+  });
+  it('should not change original array', () => {
+    const arr = ["This", "is", "a", "test"];
+    const arrCopy = arr.slice(0, arr.length);
+    middle(arr);
+    assert.deepEqual(arrCopy, arr);
+  });
+
+});
