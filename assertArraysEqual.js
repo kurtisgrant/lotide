b8d7a96ae45d55e3f492411d67762bc4b8d6539f
Kurtis Grant <kurtis.b.grant@gmail.com>
2021-11-30 02:31:24 +0000

    Refactored tests to use mocha and chai. Had to add return statements to assertArraysEqual to allow for testing

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,8 +4,10 @@ const assertArraysEqual = function(arr1, arr2) {
   const result = eqArrays(arr1, arr2);
   if (result) {
     console.log(`✅ Assertion passed: ${arr1.toString()} === ${arr2.toString()}`);
+    return true;
   } else {
     console.log(`🛑 Assertion failed: ${arr1.toString()} !== ${arr2.toString()}`);
+    return false;
   }
 };
 
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,4 +1,19 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual([2, 4, 6, 8, 'hello'], [2, 3, 6, 8, 'hello']);
-assertArraysEqual(['this', 'that', 'the other'], ['this', 'that', 'the other']);
+// assertArraysEqual([2, 4, 6, 8, 'hello'], [2, 3, 6, 8, 'hello']);
+// assertArraysEqual(['this', 'that', 'the other'], ['this', 'that', 'the other']);
+
+describe('#assertArraysEqual', () => {
+
+  it('should return false when given [3,6] and [1,6]', () => {
+    assert.isFalse(assertArraysEqual([3, 6], [1, 6]));
+  });
+  it('should return true when given [1, 5] and [1, 5]', () => {
+    assert.isTrue(assertArraysEqual([1, 5], [1, 5]));
+  });
+  it('should return true when given [] and []', () => {
+    assert.isTrue(assertArraysEqual([], []));
+  });
+
+});
